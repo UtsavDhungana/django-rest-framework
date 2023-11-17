50797f1e2c591a10603f514719bff9fac23d840f
UtsavDhungana <utsav.dhungana123@gmail.com>
2023-11-17 17:20:05 +0545

    init

---
+++ posts/views.py
@@ -0,0 +1,9 @@
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
+
+@api_view(http_method_names=["GET"])
+def homepage(request:Request):
+    response = {"message":"Hello World"}
+    return Response(data=response, status=status.HTTP_200_OK)
