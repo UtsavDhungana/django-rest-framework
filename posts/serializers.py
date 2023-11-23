efe43d206fadb5d2460dba9b646403ab628eb4dc
UtsavDhungana <utsav.dhungana123@gmail.com>
2023-11-23 14:57:21 +0545

    CRUD with api view

---
+++ posts/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from .models import *
+
+# class PostSerializer(serializers.Serializer):
+#     id = serializers.IntegerField(read_only=True)
+#     title = serializers.CharField(max_length=50)
+#     content = serializers.CharField()
+#     created = serializers.DateTimeField(read_only=True)
+
+class PostSerializer(serializers.ModelSerializer):
+    title = serializers.CharField(max_length=50)
+     
+
+    class Meta:
+        model = Post
+        fields=["id","title","content","created"]
+
